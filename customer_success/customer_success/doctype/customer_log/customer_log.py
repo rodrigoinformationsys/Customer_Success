8b6edb1d620437a91384bb2e0845371ef8f17287
Rodrigo Giacobelli <rodrigo.giacobelli@informationsys.com>
2019-09-03 19:43:59 +0000

    feat: added customer_log to customer_succeess module

---
+++ customer_success/customer_success/doctype/customer_log/customer_log.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Rodrigo Giacobelli and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class CustomerLog(Document):
+	pass
---
+++ customer_success/customer_success/doctype/customer_log/test_customer_log.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Rodrigo Giacobelli and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestCustomerLog(unittest.TestCase):
+	pass
